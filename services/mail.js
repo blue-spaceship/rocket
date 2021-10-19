4ae6584afa26e253972de09356a25c11ee99455d
Willian Rodrigues <w.almeida.w@gmail.com>
2021-10-19 14:53:25 -0300

    Fluorite Changes

--- services/mail.js
+++ services/mail.js
@@ -6,8 +6,8 @@ if(process.env.SENDGRID_API_KEY){
 
 export const Test = async () => {
   const msg = {
-    to: 'w.almeida.w@gmail.com',
-    from: 'okaywillian@gmail.com',
+    to: 'ti@minery.com.br',
+    from: 'noreply@minery.com.br',
     subject: 'Teste de Integração Sendgrid',
     content: [ { type: 'text/plain', value: "Hello world" } ]
   }
@@ -19,7 +19,7 @@ export default async (template_id, to, data) => {
   const msg = {
     template_id,
     to,
-    from: "Willian Rodrigues <okaywillian@gmail.com>",
+    from: "Willian Rodrigues <noreply@minery.com.br>",
     dynamicTemplateData: data
   }
 
@@ -29,6 +29,6 @@ export default async (template_id, to, data) => {
 }
 
 export const Templates = {
-  'newUser': 'd-dfe9679126da4674819e2d881b694caf',
-  'forgetPassword' : 'd-39fd27459bb245e4bbe1bf31612e4a79'
+  'newUser': 'd-ea0da27cffce4fdfa388c5e94a1d0495',
+  'forgetPassword' : 'd-716038270ce342e48821fcdfb990e98f'
 }
