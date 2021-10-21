a6b10dc1f180849f4f9e7faa3ca260ce9f8dd00e
Willian Rodrigues <w.almeida.w@gmail.com>
2021-10-21 06:43:31 -0300

    manifest dev fixed

--- components/dev/badge.js
+++ components/dev/badge.js
@@ -12,7 +12,7 @@ export const Badge = () => {
 
     return noProduction ? (<>
         <Head>
-            <link rel="manifest" href="/dev/manifest.json" />
+            <link rel="manifest" href="/manifest-dev.json" />
             <link rel="icon" href="/dev/assets/icons/icon-192x192.png" />
             <link rel="apple-touch-icon" href="/dev/assets/icons/icon-192x192.png" />
             <link rel="icon" sizes="256x256" href="/dev/assets/icons/icon-256x256.png" />
@@ -21,8 +21,8 @@ export const Badge = () => {
             <link rel="apple-touch-icon" sizes="384x384" href="/dev/assets/icons/icon-384x384.png" />
             <link rel="icon" sizes="512x512" href="/dev/assets/icons/icon-512x512.png" />
             <link rel="apple-touch-icon" sizes="512x512" href="/dev/assets/icons/icon-512x512.png" />
-            <meta name="theme-color" content="#08BDBD" />
-            <meta name="msapplication-navbutton-color" content="#08BDBD" />
+            <meta name="theme-color" content="#FF0077" />
+            <meta name="msapplication-navbutton-color" content="#FF0077" />
         </Head>
 
         <style jsx global>                
